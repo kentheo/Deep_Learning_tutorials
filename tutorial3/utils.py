a3516011b83b505a6de29b4d7ed0e09b0441ed80
Stylianos Moschoglou <neumann@dyn903-156.eduroam.ic.ac.uk>
2019-02-12 10:13:18 +0000

    added tutorial3.ipynb and utils.py

--- tutorial3/utils.py
+++ tutorial3/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 
+
 def interpolate(z1, z2, num=11):
     Z = np.zeros((z1.shape[0], num))
     for i in range(z1.shape[0]):
@@ -17,6 +18,12 @@ def denorm_for_tanh(x):
 
 def denorm_for_sigmoid(x):
     x = x.clamp(0, 1)
+    x = x.view(x.size(0), 1, 28, 28)
+    return x
+
+def denorm_for_binary(x):
+    x = x.clamp(0, 1)
+    x = x>0.5
     x = x.view(x.size(0), 1, 28, 28)
     return x
 
